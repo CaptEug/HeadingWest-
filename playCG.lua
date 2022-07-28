85e4bbbebfba2f3f6ee33c8722d9970d3fa61525
CaptEug <94046599+CaptEug@users.noreply.github.com>
2022-07-28 20:07:41 +0800

    a

--- playCG.lua
+++ playCG.lua
@@ -1,30 +1,29 @@
 CG = {}
+CG.__index = CG
 
 
 
-
-function CG.new(name)
-    local instance = setmetatable({}, CG)
-    instance.video = love.video.newVideo("")
-end
+CG1 = love.graphics.newVideo('OP.ogv')
 
 
-function CG:playCG()
+
+function CG:playCG(dt)
+    self:play()
     if love.graphics.getWidth() / love.graphics.getHeight() < 720 / 480 then
         love.graphics.draw(
-            self.video,
+            self,
             0,
-            love.graphics.getHeight() / 2 - (heightVideo * love.graphics.getWidth())/ (2 * widthVideo),
+            love.graphics.getHeight() / 2 - (480 * love.graphics.getWidth())/ (2 * 720),
             0,
-            love.graphics.getWidth() / widthVideo
+            love.graphics.getWidth() / 720
         )   
     else
         love.graphics.draw(
-            video,
-            love.graphics.getWidth() / 2 - (widthVideo * love.graphics.getHeight())/ (2 * heightVideo),
+            self,
+            love.graphics.getWidth() / 2 - (720 * love.graphics.getHeight())/ (2 * 480),
             0,
             0,
-            love.graphics.getHeight() / heightVideo
+            love.graphics.getHeight() / 480
         )
     end
 end
