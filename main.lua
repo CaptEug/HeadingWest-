6d942d4be87c07f5fff04753135536b6031d7c51
CaptEug <94046599+CaptEug@users.noreply.github.com>
2022-07-29 17:26:25 +0800

    heng

--- main.lua
+++ main.lua
@@ -22,34 +22,19 @@ function love.update(dt)
     Berlin:update(dt)
     
     if love.keyboard.isDown("w") then
-        Center.y = Center.y - 5
+        cam:move(0,-10)
     end
     if love.keyboard.isDown("s") then
-        Center.y = Center.y + 5
+        cam:move(0,10)
     end
     if love.keyboard.isDown("a") then
-        Center.x = Center.x - 5
+        cam:move(-10,0)
     end
     if love.keyboard.isDown("d") then
-        Center.x = Center.x + 5
+        cam:move(10,0)
     end
         
-    cam:lookAt(Center.x, Center.y)
-    
-    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
-    if Center.x < w / 2 then
-        Center.x = w / 2
-    end
-    if Center.y < h / 2 then
-        Center.y = h / 2
-    end
-    local mapw, maph = gamemap.width * gamemap.tilewidth, gamemap.height * gamemap.tileheight
-    if Center.x > (mapw - w / 2) then
-        Center.x =(mapw - w / 2) 
-    end
-    if Center.y > (maph - h / 2) then
-        Center.y =(maph - h / 2)
-    end 
+
     
     function love.wheelmoved(x, y)
         if y > 0 then
