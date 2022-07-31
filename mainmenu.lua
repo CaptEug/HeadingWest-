3dc401b1ee7f04d6d3ad9632a9bfd1e2dbddf988
CaptEug <94046599+CaptEug@users.noreply.github.com>
2022-07-31 16:25:14 +0800

    red

--- mainmenu.lua
+++ mainmenu.lua
@@ -4,7 +4,7 @@ mainmenu = {}
 
 function mainmenu:load()
     Europe = love.graphics.newImage('Europe.png')
-    love.graphics.setFont(love.graphics.newFont('Russian.ttf', 50))
+    love.graphics.setFont(love.graphics.newFont('Russian.ttf', 100))
 end
 
 
@@ -15,8 +15,10 @@ end
 
 
 function mainmenu:draw()
+    love.graphics.setColor(1, 0, 0)
     cam:attach()
         love.graphics.draw(Europe, 0, 0)
     cam:detach()
-    love.graphics.print("НА ЗАПАД!", love.graphics.getWidth() / 2, love.graphics.getHeight() / 4)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("НА ЗАПАД!", (love.graphics.getWidth() / 2)-170, love.graphics.getHeight() / 4)
 end
