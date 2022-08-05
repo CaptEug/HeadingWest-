be07c95822f069a07c2d1850e0be54487b6b80d6
ToD <davenportcaster@yahoo.com>
2022-08-05 23:21:36 +0800

    h

--- loadmenu.lua
+++ loadmenu.lua
@@ -65,6 +65,7 @@ function createsave(filenum)
     data.tankangle=0
     data.filenumber=filenum
 end
+
 function filesave (filenum)
     local number=filenum
     data.stage="testmap"
@@ -73,12 +74,11 @@ function filesave (filenum)
     data.tankangle=tanks.angle
 
     if number ==1 then
-        love.filesystem.write("file1.lua", table.show(), size (number))
-    else if filenum ==2 then 
-        love.filesystem.write("file2.lua", data (string), size (number))
-        else
-        love.filesystem.write("file3.lua", data (string), size (number))
-        end
+        love.filesystem.write("file1.lua", table.show(data,"data"))
+    elseif number ==2 then 
+        love.filesystem.write("file2.lua", table.show(data,"data"))
+    elseif number ==3 then
+        love.filesystem.write("file3.lua", table.show(data,"data"))
     end
 end
 
@@ -86,11 +86,10 @@ function fileload (filenum)
     local number=filenum
 
     if number ==1 then
-        love.filesystem.write("file1.lua", data (string), size (number))
-    else if filenum ==2 then 
-        love.filesystem.write("file2.lua", data (string), size (number))
-        else
-        love.filesystem.write("file3.lua", data (string), size (number))
-        end
+        love.filesystem.load("file1.lua")
+    elseif number ==2 then 
+        love.filesystem.load("file2.lua")
+    elseif number ==3 then
+        love.filesystem.load("file3.lua")
     end
 end
