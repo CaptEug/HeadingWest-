398bf41ce6cdb8654e10a4341ac657e550f57c2e
ToD <davenportcaster@yahoo.com>
2022-08-06 17:10:13 +0800

    l

--- Saving.lua
+++ Saving.lua
@@ -2,42 +2,29 @@ Saving={}
 Data={}
 
 function Saving:createsave(filenum)
-    Data.stage=0
-    Data.tankx=0
-    Data.tanky=0
-    Data.tankangle=0
     Data.filenumber=filenum
+    Data.stage=0
+    Data.tankdata={}
+    
 end
 
 function Saving:filesave (filenum)
     local number=filenum
+    local file={"file1.lua","file2.lua","file3.lua"}
     Data.stage="testmap"
     Data.tankx=tanks.x
     Data.tanky=tanks.y
     Data.tankangle=tanks.angle
 
-    if number ==1 then
-        love.filesystem.write("file1.lua", table.show(Data,"data"))
-    elseif number ==2 then 
-        love.filesystem.write("file2.lua", table.show(Data,"data"))
-    elseif number ==3 then
-        love.filesystem.write("file3.lua", table.show(Data,"data"))
-    end
+    love.filesystem.write(file[number], table.show(Data,"data"))
 end
 
 function Saving:fileload (filenum)
     local number=filenum
+    local file={"file1.lua","file2.lua","file3.lua"}
 
-    if number ==1 then
-        love.filesystem.load("file1.lua")
-        Saving:getdata()
-    elseif number ==2 then 
-        love.filesystem.load("file2.lua")
-        Saving:getdata()
-    elseif number ==3 then
-        love.filesystem.load("file3.lua")
-        Saving:getdata()
-    end
+    love.filesystem.load(file[number])
+    Saving:getdata()
 end
 
 function Saving:getdata()
