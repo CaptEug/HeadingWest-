24d3c88b6241a5dd701455e1a1d2132e17e711af
ToD <davenportcaster@yahoo.com>
2022-08-08 19:16:48 +0800

    l

--- Saving.lua
+++ Saving.lua
@@ -2,7 +2,32 @@ Saving={}
 require 'libraries/show'
 Data={}
 function Saving:createsave(filenum)
-    
+    local number=filenum
+    local file={"file1.lua","file2.lua","file3.lua"}
+    local filedata={}
+    filedata.filenumber=filenum
+    filedata.stage="testmap"
+    filedata.tank_name="mause"
+    filedata.x=0
+    filedata.y=0
+    filedata.angle=0
+    filedata.turret_angle=0
+    filedata.turret_path="tanks/german/MAUS/MAUS turret.png"
+    filedata.bodywork_path="tanks/german/MAUS/MAUS hull.png"
+    filedata.turret_offset=27
+    filedata.maxspeed=200
+    filedata.back_maxspeed=40
+    filedata.acceleration=30
+    filedata.back_acceleration=20
+    filedata.stop_acceleration=150
+    filedata.max_Rotation_speed=0.8
+    filedata.Rotational_acceleration=0.3
+    filedata.stop_rotation_ac=0.7
+    filedata.turret_max_Rotation_speed=0.5
+    filedata.turret_Rotational_acceleration=0.4
+    filedata.turret_stop_rotation_ac=0.8
+
+    love.filesystem.write(file[number], table.show(filedata,'Data'))
 end
 
 function Saving:filesave (filenum)
@@ -38,13 +63,17 @@ function Saving:fileload (filenum)
     Gamestate.switch(testmap)
     local number=filenum
     local file={"file1.lua","file2.lua","file3.lua"}
-    love.filesystem.getInfo(file[number])
+    if love.filesystem.getInfo(file[number])==nil then
+        Saving:createsave(filenum)
+    end
     Data=love.filesystem.load("file1.lua")
 end
 
 function Saving:getdata(filenum)
     local number=filenum
     local file={"file1.lua","file2.lua","file3.lua"}
-    love.filesystem.getInfo(file[number])
+    if love.filesystem.getInfo(file[number])==nil then
+        Saving:createsave(filenum)
+    end
     Data=love.filesystem.load("file1.lua")
 end
