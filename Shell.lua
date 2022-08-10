1b0237dff9015f0a71c30b955778d1919644ad8c
CaptEug <94046599+CaptEug@users.noreply.github.com>
2022-08-10 16:51:16 +0800

    shit

--- Shell.lua
+++ Shell.lua
@@ -2,10 +2,10 @@ Shell = {}
 
 function createshell()
     local speed = 256
-    local mx, my = love.mouse.getPosition()
+    local mx, my = cam:mousePosition(ox,oy,w,h)
     local angle = math.atan2(my - MAUS1.y, mx - MAUS1.x)
     local vx, vy = math.cos(angle) * speed,
                    math.sin(angle) * speed
-    local shell = world:newRectangleCollider(MAUS1.x, MAUS1.y, 10, 10)
+    local shell = world:newRectangleCollider(MAUS1.x + 100, MAUS1.y + 100, 10, 10)
     shell:setLinearVelocity(vx, vy)
 end
