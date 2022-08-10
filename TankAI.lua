052eb1f336f2d54007572c956020da9effef75ce
ToD <davenportcaster@yahoo.com>
2022-08-10 16:55:45 +0800

    w

--- TankAI.lua
+++ TankAI.lua
@@ -1,7 +1,28 @@
 TankAI={}
 
 function TankAI:spawn()
-    
+    MAUS1 = tanks.new(
+        Data.tank_name,
+        Data.x,
+        Data.y,
+        Data.angle,
+        Data.turret_angle,
+        Data.turret_path,
+        Data.bodywork_path,
+        Data.turret_offset,
+        Data.maxspeed,
+        Data.back_maxspeed,
+        Data.acceleration,
+        Data.back_acceleration,
+        Data.stop_acceleration,
+        Data.max_Rotation_speed,
+        Data.Rotational_acceleration,
+        Data.stop_rotation_ac,
+        Data.turret_max_Rotation_speed,
+        Data.turret_Rotational_acceleration,
+        Data.turret_stop_rotation_ac
+    )
+    MAUS1:create()
 end
 
 function TankAI:find()
@@ -10,4 +31,8 @@ end
 
 function TankAI:move()
     
+end
+
+function TankAI:update()
+    MAUS1:use()
 end
