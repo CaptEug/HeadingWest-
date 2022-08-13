e7bf849cfd7a66d52664919411b346f38fbaf12c
CaptEug <94046599+CaptEug@users.noreply.github.com>
2022-08-13 18:03:54 +0800

    NICE sounftrack

--- Tank.lua
+++ Tank.lua
@@ -3,6 +3,8 @@ tanks = {}
 tank = {}
 tank.__index = tanks
 
+MAUS_tracksound = love.audio.newSource('music/sound/Heavy-truck-driving-sound-loop.mp3', "stream")
+
 function tanks.new(
     tank_name,
     x,
@@ -76,7 +78,7 @@ end
 
 function tanks:create()
     MAUS_Ammo = Ammo.new()
-    APCBC_128mm = Ammo.newShell(2560,0,MAUS_Ammo)
+    APCBC_128mm = Ammo.newShell(25600,0,MAUS_Ammo)
     Reload_time = 3
     Reload_timer = 0
 
@@ -95,6 +97,7 @@ function tanks:create()
 end
 
 function tanks:move(dt)
+    ismove = false
     Reload_timer = Reload_timer - dt
     if love.mouse.isDown(1) and Reload_timer <= 0 then
         MAUS_Ammo:shoot(APCBC_128mm, APCBC)
@@ -102,6 +105,7 @@ function tanks:move(dt)
     end
 
     if love.keyboard.isDown('up') then
+        ismove = true
         if self.speed<self.maxspeed then
             self.speed=self.speed+self.acceleration*dt
         end
@@ -115,6 +119,7 @@ function tanks:move(dt)
     end
 
     if love.keyboard.isDown('down') then
+        ismove = true
         if self.speed>-self.back_maxspeed then
             self.speed=self.speed-self.back_acceleration*dt
         end
@@ -128,6 +133,7 @@ function tanks:move(dt)
     end
 
     if love.keyboard.isDown('left') then
+        ismove = true
         if self.Rotational_speed>-self.max_Rotation_speed then
             self.Rotational_speed=self.Rotational_speed-self.Rotational_acceleration*dt
         end
@@ -141,6 +147,7 @@ function tanks:move(dt)
     end
 
     if love.keyboard.isDown('right') then
+        ismove = true
         if self.Rotational_speed<self.max_Rotation_speed then
             self.Rotational_speed=self.Rotational_speed+self.Rotational_acceleration*dt
         end
@@ -151,6 +158,14 @@ function tanks:move(dt)
             end
             self.Rotational_speed=self.Rotational_speed-self.stop_rotation_ac*dt
         end
+    end
+
+    if ismove then
+        love.audio.play(MAUS_tracksound)
+        cam:move(math.random(-1,1),math.random(-1,1))
+    end
+    if not ismove then
+        love.audio.pause(MAUS_tracksound)
     end
 
     cos = math.cos(self.angle)
