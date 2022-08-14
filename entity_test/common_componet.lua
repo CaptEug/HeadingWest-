ee108277c0bdaec90dcc19772d5ba2b58b80d1f3
YZMYT <1563400181@qq.com>
2022-08-14 18:07:04 +0800

    dw

--- entity_test/common_componet.lua
+++ entity_test/common_componet.lua
@@ -11,6 +11,32 @@ return{
         return location
     end,
 
+    move = function(speed, angle)
+        local move = Component.new "move"
+        move.speed = speed
+        move.angle = angle
+        return move
+    end,
+
+    rotation_data = function(Rotation_speed, max_Rotation_speed, Rotational_acceleration, stop_rotation_ac)
+        local r = Component.new "rotation_data"
+        r.Rotational_speed = Rotation_speed
+        r.max_Rotational_speed = max_Rotation_speed
+        r.Rotational_acceleration = Rotational_acceleration
+        r.stop_rotation_ac = stop_rotation_ac
+        return r
+    end,
+
+    move_data = function(maxspeed, back_maxspeed, acceleration, back_acceleration, stop_acceleration)
+        local m = Component.new "move_data"
+        m.maxspeed = maxspeed
+        m.back_maxspeed = back_maxspeed
+        m.acceleration = acceleration
+        m.back_acceleration = back_acceleration
+        m.stop_acceleration = stop_acceleration
+        return m
+    end,
+
     new_bodywork = function(bodywork_path)
         local bodywork = Component.new "bodywork"
         bodywork.picture = love.graphics.newImage(bodywork_path)
@@ -23,6 +49,78 @@ return{
         local functional = Component.new "functional"
         functional.fn = fn
         return functional
+    end,
+
+    init = function(fn)
+        local functiona = Component.new "init"
+        functiona.fn = fn
+        return functiona
+    end,
+
+    movetype = function(self, dt)
+        local bodywork = self:get "bodywork"
+        local move = self:get "move"
+        local r = self:get "rotation_data"
+        local m = self:get "move_data"
+        local cos = math.cos(move.angle)
+        local sin = math.sin(move.angle)
+        local vx =  move.speed * sin
+        local vy =  move.speed * cos * -1
+        
+        if love.keyboard.isDown('up') then
+            if move.speed<m.maxspeed then
+                move.speed = move.speed + m.acceleration*dt
+            end
+        else
+            if move.speed>0 then
+                if move.speed>-0.1 and move.speed<0.1 then
+                    move.speed = 0
+                end
+                move.speed = move.speed - m.stop_acceleration*dt
+            end
+        end
+    
+        if love.keyboard.isDown('down') then
+            if move.speed>-m.back_maxspeed then
+                move.speed = move.speed - m.back_acceleration*dt
+            end
+        else
+            if move.speed<0 then
+                if move.speed>-0.1 and move.speed<0.1 then
+                    move.speed = 0
+                end
+                move.speed = move.speed + m.stop_acceleration*dt
+            end
+        end
+
+        if love.keyboard.isDown('left') then
+            if r.Rotational_speed>-r.max_Rotational_speed then
+                r.Rotational_speed = r.Rotational_speed-r.Rotational_acceleration*dt
+            end
+        else
+            if r.Rotational_speed<0 then
+                if r.Rotational_speed>-0.1 and r.Rotational_speed<0.1 then
+                    r.Rotational_speed = 0
+                end
+                r.Rotational_speed=r.Rotational_speed+r.stop_rotation_ac*dt
+            end
+        end
+
+        if love.keyboard.isDown('right') then
+            if r.Rotational_speed<r.max_Rotational_speed then
+                r.Rotational_speed=r.Rotational_speed+r.Rotational_acceleration*dt
+            end
+        else
+            if r.Rotational_speed>0 then
+                if r.Rotational_speed>-0.1 and r.Rotational_speed<0.1 then
+                    r.Rotational_speed = 0
+                end
+                r.Rotational_speed=r.Rotational_speed-r.stop_rotation_ac*dt
+            end
+        end
+
+        bodywork.hitbox:setLinearVelocity(vx, vy)
+        bodywork.hitbox:setAngularVelocity(r.Rotational_speed)
     end
 
 }
---
+++ entity_test/tanks_new.lua
@@ -0,0 +1,67 @@
+local t_world = require 'world'
+local Component = require "tank_component"
+local coms = require  "common_componet"
+local syfn = require  "tank_sy_fn"
+wf = require 'windfield'
+location_t = coms.new_location
+bodywork_t = coms.new_bodywork
+move_t = coms.move
+rotation_data_t = coms.rotation_data
+move_data_t = coms.move_data
+keybroadcontrol = coms.movetype
+
+
+world = wf.newWorld(0, 0)
+
+Tanks = {}
+Tanks.__index = Tanks
+
+local qun = {}
+
+
+function stringToTable(str)
+    local ret = loadstring("return "..str)()
+    return ret
+end
+
+function isInArray(t, val)
+	for _, v in ipairs(t) do
+		if v == val then
+			return v
+		end
+	end
+	return false
+end
+
+function Tanks.new()
+    local instance = setmetatable({}, Tanks)
+    return instance
+end
+
+function Tanks:newtank(data, x, y)
+    t_world:register(syfn.new_renderer_system())
+    t_world:register(syfn.new_functional_system())
+    local a,b = love.filesystem.read( data, all )
+    local h = stringToTable(a)
+    local entity = t_world:assemble(h)
+    table.insert(qun, entity)
+    local body = entity:get "bodywork"
+    body.hitbox:setX(x)
+    body.hitbox:setY(y)
+    return entity
+end
+
+function Tanks:addai(entity, ai)
+    local test = isInArray(qun, entity)
+    test:add(coms.functional(ai))
+end
+     
+function Tanks:update(dt)
+    t_world:update(dt)
+    world:update(dt)
+end
+
+function Tanks:draw()
+    t_world:draw()
+    world:draw()
+end
