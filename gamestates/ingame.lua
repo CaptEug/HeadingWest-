692057587c50f3122346df2bda1a23fc6c37f274
ToD <davenportcaster@yahoo.com>
2022-08-17 23:08:25 +0800

    g

--- gamestates/ingame.lua
+++ gamestates/ingame.lua
@@ -24,29 +24,6 @@ function ingame:init()
 
     loadMap('checkpointC')
     
-    --[[MAUS1 = tanks.new(
-        Data.tank_name,
-        Data.x,
-        Data.y,
-        Data.angle,
-        Data.turret_angle,
-        Data.turret_path,
-        Data.bodywork_path,
-        Data.turret_offset,
-        Data.maxspeed,
-        Data.back_maxspeed,
-        Data.acceleration,
-        Data.back_acceleration,
-        Data.stop_acceleration,
-        Data.max_Rotation_speed,
-        Data.Rotational_acceleration,
-        Data.stop_rotation_ac,
-        Data.turret_max_Rotation_speed,
-        Data.turret_Rotational_acceleration,
-        Data.turret_stop_rotation_ac
-    )
-    MAUS1:create()
-    TankAI:create('M48A1')]]--
     m = Tanks.new()
     tank1 = m:newtank("entity_test/shushu.txt", Data.ax , Data.ay)
     m:addai(tank1, keybroadcontrol)
