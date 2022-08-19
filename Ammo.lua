9e783a6f1253c39976e016b4fd7331a9585733f2
YZMYT <1563400181@qq.com>
2022-08-19 23:29:24 +0800

    f

--- Ammo.lua
+++ Ammo.lua
@@ -22,11 +22,11 @@ end
 function Ammo:shoot(shell_name,shell_type,shell_table)
     local mx, my = cam:mousePosition()
     local tur = tank1:get "turret_rdata"
-    local body = tank1:get "location"
+    local body = tank1:get "turret"
     --local angle = math.atan2(tur.angle)
-    local vx, vy = math.cos(tur.angle) * shell_name.speed,
-                   math.sin(tur.angle) * shell_name.speed
-    local shell = world:newRectangleCollider(body.x, body.y - 100, 10, 10)
+    local vx, vy = math.sin(tur.angle) * shell_name.speed,
+                   -math.cos(tur.angle) * shell_name.speed
+    local shell = world:newRectangleCollider(body.AMx-5, body.AMy-5, 10, 10)
     --local shell_type = shell_type
     shell:setCollisionClass(shell_type)
     shell:setRestitution(0.8)
@@ -37,7 +37,7 @@ end
 function Ammo.draw()
     for i, shell in ipairs(APCBC) do
         local sx, sy = shell:getPosition()
-        love.graphics.rectangle("fill", sx, sy, 100, 100)
+        --love.graphics.rectangle("fill", sx, sy, 100, 100)
         
     end
     for i, shell in ipairs(HEAT) do
