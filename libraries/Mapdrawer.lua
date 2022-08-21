68dcb9c1dd82ae78c815502d29343dd911bf6ee2
CaptEug <94046599+CaptEug@users.noreply.github.com>
2022-08-22 00:06:20 +0800

    sd

--- libraries/Mapdrawer.lua
+++ libraries/Mapdrawer.lua
@@ -25,7 +25,7 @@ function loadMap(mapName)
         end
     end
 
-    tank1 = tanks_table:newtank("entity_test/shushu.txt", Data.ax , Data.ay, Data.angle)
+    tank1 = tanks_table:newtank("entity_test/MAUS.txt", Data.ax , Data.ay, Data.angle)
     tank2=tanks_table:newtank("entity_test/M48A1.txt", Data.ax , Data.ay, Data.angle)
     tanks_table:addai(tank1, keybroadcontrol)
     tanks_table:addai(tank2, keybroadcontrol)
