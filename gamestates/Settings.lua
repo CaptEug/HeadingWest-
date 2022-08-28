a248d09e52a1bed787b0a403ffe149d66d15d2e9
ToD <davenportcaster@yahoo.com>
2022-08-29 00:12:41 +0800

    d

--- gamestates/Settings.lua
+++ gamestates/Settings.lua
@@ -2,21 +2,32 @@ Settings = {}
 Settings = Gamestate.new()
 
 function Settings:init()
-    Sbutton=buttons.new()
+    Sbuttons=buttons.new()
     Apply=buttons.newButton(
-        'Apply',
+        "Apply",
         function ()
-            
-        end
+            Gamestate.pop()
+        end,
+        Sbuttons
+    )
+    Back=buttons.newButton(
+        "Back",
+        function ()
+            Gamestate.switch(ingame)
+        end,
+        Sbuttons
+    )
 
-)
 end
 
-function Settings:update(dt)
+function Settings:update()
     local ww, wh = love.graphics.getDimensions()
-
+    Apply.bx=ww/2
+    Apply.by=wh/2
+    Back.bx=100
+    Back.by=100
 end
 
 function Settings:draw()
-
+    Sbuttons:use()
 end
