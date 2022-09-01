3e98dfa6d626e29bb3a4ec657021fa0775ebced4
ToD <davenportcaster@yahoo.com>
2022-09-01 17:38:08 +0800

    d

--- Saving.lua
+++ Saving.lua
@@ -45,3 +45,31 @@ function Saving:getdata(filenum)
     end
     Data=love.filesystem.load(file[filenum])
 end
+
+function Saving:create_settings()
+    local filedata={}
+    filedata.ww=800
+    filedata.wh=800
+    filedata.volume=1
+   
+    love.filesystem.write("Setting_data", table.show(filedata,'Setting_data'))
+end
+function Saving:save_settings()
+    local ww,wh=love.graphics.getDimensions()
+    local filedata={}
+    filedata.ww=ww
+    filedata.wh=wh
+    filedata.volume=love.audio.getVolume()
+    love.filesystem.write("Setting_data", table.show(filedata,'Setting_data'))
+end
+
+function Saving:load_settings()
+
+    if love.filesystem.getInfo("Setting_data")==nil then
+        Saving:createsave("Setting_data")
+    end
+    local Setting_data=love.filesystem.load("Setting_data")
+    Setting_data()
+    love.audio.setVolume(Setting_data.volume)
+    love.window.setMode(Setting_data.ww,Setting_data.wh)
+end
