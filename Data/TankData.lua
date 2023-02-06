0cbdfdce737d8a46ea0240bb73bdc41dce64052a
Eug <capteug324@gmail.com>
2023-02-06 13:19:34 +0800

    tankdata update

--- Data/TankData.lua
+++ Data/TankData.lua
@@ -6,6 +6,8 @@ T72A_line = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_line.png')
 T72A_turret = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_turret.png')
 T72A_hull = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_hull.png')
     --accessory
+    T72A_smokegernate_line = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_smokegernade_line.png')
+    T72A_smokegernate_turret = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_smokegernade_turret.png')
     T72A_kontakt1_line = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_kontakt1_line.png')
     T72A_kontakt1_turret = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_kontakt1_turret.png')
     T72A_kontakt1_hull = love.graphics.newImage('Assets/tanks/soviet/T-72/T72A_kontakt1_hull.png')
@@ -28,11 +30,19 @@ function TankData:load()
         name = 'T-72a',
         width = 64,
         length = 127,
+        weight = 41.5,
         line_image = T72A_line,
         turret_image = T72A_turret,
         hull_image = T72A_hull,
-        accessories = { 
+        accessories = {
+            --armor
+            {name = 'Base_Armor', line_image = T72A_smokegernate_line, turret_image = T72A_smokegernate_turret, hull_image = Blank_line, tag = 'Armor'},
             {name = 'Kontakt_1', line_image = T72A_kontakt1_line, turret_image = T72A_kontakt1_turret, hull_image = T72A_kontakt1_hull, tag = 'Armor'},
+            --aim
+            --ammo
+            {name = '3BM22', line_image = Blank_line},
+            --mob
+            {name = 'V12_diesel', line_image = Blank_line, hp = 780, tag = 'Mob' }
         },
         buildtime = 2
     }
@@ -42,12 +52,22 @@ function TankData:load()
         name = 'T-72b',
         width = 64,
         length = 127,
+        weight = 44.5,
         line_image = T72B_line,
         turret_image = T72B_turret,
         hull_image = T72B_hull,
-        accessories = { 
+        accessories = {
+            --armor
+            {name = 'Base_Armor', line_image = Blank_line, turret_image = Blank_line, hull_image = Blank_line, tag = 'Armor'},
             {name = 'Kontakt_1', line_image = T72B_kontakt1_line, turret_image = T72B_kontakt1_turret, hull_image = T72B_kontakt1_hull,tag = 'Armor'},
-            {name = 'Kontakt_5', line_image = T72B_kontakt5_line, turret_image = T72B_kontakt5_turret, hull_image = T72B_kontakt5_hull,tag = 'Armor'}
+            {name = 'Kontakt_5', line_image = T72B_kontakt5_line, turret_image = T72B_kontakt5_turret, hull_image = T72B_kontakt5_hull,tag = 'Armor'},
+            --aim
+            --ammo
+            {name = '3BM22', line_image = Blank_line},
+            {name = '3BM42', line_image = Blank_line},
+            --mob
+            {name = 'V12_diesel', line_image = Blank_line, hp = 780, tag = 'Mob' },
+            {name = 'V_92S2F', line_image = Blank_line, hp = 1130, tag = 'Mob' }
         },
         buildtime = 2
     }
