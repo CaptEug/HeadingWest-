eb5c07a5133473c69a82ab276a8bcc3905c221a6
CaptEug <94046599+CaptEug@users.noreply.github.com>
2023-02-16 17:37:24 +0800

    no

--- UI/TankInfoPanel.lua
+++ UI/TankInfoPanel.lua
@@ -1,5 +1,29 @@
 TankInfoPanel = {}
 
 function TankInfoPanel:load()
+    --add tankinfopanel button
+    for i,tank in ipairs(CurrentPlace.tankstock) do
+        tank.Infobutton = buttons.new()
+        local button = buttons.newCamButton(
+            invisible_button,
+            function ()
+                TankPanelopen = true
+                TankInfoPanel(tank)
+            end,
+            tank.Infobutton
+        )
+    end
+end
+
+function TankInfoPanel:update()
+    --tankbutton pos update
+    for i, tank in ipairs(CurrentPlace.tankstock) do
+        for i, button in ipairs(tank.Infobutton) do
+            button.x, button.y = tank.hullcollider:getPosition()
+        end
+    end
+end
+
+function TankInfoPanel:draw()
     
 end
