f7918ca73a071e1b01ce7ca8dacaebb04e5b8317
CaptEug <94046599+CaptEug@users.noreply.github.com>
2023-03-05 22:23:46 +0800

    small change on tank update

--- libraries/TankSpawner.lua
+++ libraries/TankSpawner.lua
@@ -56,8 +56,8 @@ end
 
 function TankSpawner:update(dt)
     for i, tank in ipairs(CurrentPlace.exsist_tank) do
-        tank.functions.move(tank,dt)
         tank.functions.update(tank)
+        tank.functions.move(tank,dt) 
     end
 end
 
