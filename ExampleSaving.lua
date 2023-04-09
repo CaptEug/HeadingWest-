e17f223f466cd0b2bd2611a78edfab19309a6d0b
ToD <davenportcaster@yahoo.com>
2023-04-09 11:10:07 +0800

    tabletostring

---
+++ ExampleSaving.lua
@@ -0,0 +1,38 @@
+--[[Data = {
+    ["OilProduction"] = 0;
+    ["ax"] = 200;
+    ["ay"] = 1600;
+    ["SteelProduction"] = 0;
+    ["filenumber"] = 1;
+    ["Steel"] = 10000;
+    ["Oil"] = 10000;
+    ["angle"] = 0;
+ };--]]
+
+ FileData = {
+
+    Filenumber = 1;
+
+    Resources = {
+        Oil = 10000;
+        OilProduction = 0;
+        Steel = 10000;
+        SteelProduction =0;
+        Resources = {
+            Oil = 10000;
+            OilProduction = 0;
+            Steel = 10000;
+            SteelProduction =0;
+        };
+    };
+
+    EventProgress = {
+        Progress=1;
+
+    };
+
+    ExistTanks = {
+
+    }
+
+ }
