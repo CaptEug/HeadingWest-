d1f7b4ffa744107efe59c3d8eacb524a7a6f1d6d
ToD <davenportcaster@yahoo.com>
2023-07-23 01:24:37 +0800

    prebuild

--- Data/Constructure/ConstructMenu.lua
+++ Data/Constructure/ConstructMenu.lua
@@ -49,14 +49,15 @@ function ConstructMenu:update(dt)
     if love.mouse.isDown(2) and ConstructurePicked then
         ConstructurePicked = false
         ConstructMenu.build = false
-        self.query = false
         ConstructureSelected = {}
     end
     for i, constructure in ipairs(ConstructionQueue) do
-        constructure.buildtime = constructure.buildtime - dt
-        if constructure.buildtime <= 0 then
-            
-            BuildConstructure(CurrentPlace, table.remove(ConstructionQueue, i), 'friendly', constructure.x, constructure.y)
+        constructure.building.buildtime = constructure.building.buildtime - dt
+        if constructure.building.buildtime <= 0 then
+            constructure.preBuild:destroy()
+            constructure.preBuild = nil
+            BuildConstructure(CurrentPlace, constructure.building, 'friendly', constructure.building.x, constructure.building.y)
+            table.remove(ConstructionQueue,i)
         end
     end
     if ConstructurePicked == true then
@@ -97,7 +98,8 @@ function ConstructMenu:draw()
         love.graphics.draw(ConstructureSelected.image, x + ConstructureSelected.width/(2/cam.scale), y + ConstructureSelected.length/(2/cam.scale), 0, cam.scale, cam.scale, imagewidth/2, imagelength/2)
     end
 --draw green building preview
-    for i, building in ipairs(ConstructionQueue) do
+    for i, build in ipairs(ConstructionQueue) do
+        local building = build.building
         local x, y = cam:cameraCoords(building.x, building.y)
         local imagewidth, imagelength = building.image:getWidth(), building.image:getHeight()
         love.graphics.setColor(0,179/255,0)
@@ -115,8 +117,10 @@ function BuildDetact(button)
         local x, y = IntX, IntY
         local imagewidth = ConstructureSelected.image:getWidth()
         building.x, building.y = x, y
-        local preBuild = CurrentPlace.world:newRectangleCollider(x,y,ConstructureSelected.width,ConstructureSelected.length)
-        table.insert(ConstructMenu.PreBuild, preBuild)
-        table.insert(ConstructionQueue, building)
+        local preBuild = CurrentPlace.world:newRectangleCollider(x-(ConstructureSelected.preBuild.width-ConstructureSelected.width)/2,y-(ConstructureSelected.preBuild.length-ConstructureSelected.length)/2,ConstructureSelected.preBuild.width,ConstructureSelected.preBuild.length)
+        local construt = {}
+        construt.building = building
+        construt.preBuild = preBuild
+        table.insert(ConstructionQueue, construt)
     end
 end
