a47845f8c8de68f3139c1e0baee228b4f3e87a54
Eug <capteug324@gmail.com>
2023-07-22 20:49:09 +0800

    ыыы

--- libraries/utilities/windows.lua
+++ libraries/utilities/windows.lua
@@ -1,4 +1,5 @@
 Window = {}
+Windows = {}
 Window.__index = Window
 
 function Window.new()
@@ -8,13 +9,40 @@ function Window.new()
         w = 640,
         h = 64,
         dragging = false,
-        active = false
+        active = false,
+        buttons = Buttons.new()
     }
 
     instance = setmetatable({}, Window)
+    table.insert(Windows, instance)
     return instance
 end
 
 function Window:use()
     
+end
+
+function Window:mousepressed()
+    -- Check if the mouse is inside the TDscreen.window
+    if x >= self.x and x <= self.x + self.w and
+     y >= self.y and y <= self.y + self.h then
+        Cursormode = 'dragging'
+        self.dragging = true
+       -- Calculate the offset between the mouse position and the TDscreen.window position
+       self.offsetX = x - self.x
+       self.offsetY = y - self.y
+    end
+end
+
+function Window:mousereleased()
+    -- Stop dragging when the mouse is released
+    self.dragging = false
+end
+
+function Window:mousemoved()
+    -- Update the TDscreen.window position if the user is dragging it
+    if self.dragging then
+        self.x = x - self.offsetX
+        self.y = y - self.offsetY
+    end
 end
