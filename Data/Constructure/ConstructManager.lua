5e475f7f0dadb998093fc56c695ad1e7ef112608
ToD <davenportcaster@yahoo.com>
2023-07-27 15:39:55 +0800

    new prebuild

--- Data/Constructure/ConstructManager.lua
+++ Data/Constructure/ConstructManager.lua
@@ -2,22 +2,22 @@ ConstructManager = {}
 
 function ConstructManager:query(object)
     local canBuild = true
-    local width, length = object.width - 2, object.length - 2
+    local width, length = object.preBuild.width - 2, object.preBuild.length - 2
     local x = IntX + 1
     local y = IntY + 1
     for i, obj in ipairs(CurrentPlace.exsist_building) do
         local objX = obj.location.x
         local objY = obj.location.y
-        if x >= objX and x <= objX + obj.width and y >= objY and y <= objY + obj.length then
+        if x >= objX and x <= objX + obj.preBuild.width and y >= objY and y <= objY + obj.preBuild.length then
             canBuild = false
         end
-        if  x + width >= objX and x + width <= objX + obj.width and y >= objY and y <= objY + obj.length then
+        if  x + width >= objX and x + width <= objX + obj.preBuild.width and y >= objY and y <= objY + obj.preBuild.length then
             canBuild = false
         end
-        if x + width >= objX and x + width <= objX + obj.width and y + length >= objY and y + length <= objY + obj.length then
+        if x + width >= objX and x + width <= objX + obj.preBuild.width and y + length >= objY and y + length <= objY + obj.preBuild.length then
             canBuild = false
         end
-        if  x >= objX and x <= objX + obj.width and y + length >= objY and y + length <= objY + obj.length then
+        if  x >= objX and x <= objX + obj.preBuild.width and y + length >= objY and y + length <= objY + obj.preBuild.length then
             canBuild = false
         end
         if canBuild ==false then
