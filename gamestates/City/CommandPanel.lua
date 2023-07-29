4aa6e9407e804961eebba09fda661f1deaa8c7fd
YZMYT <1563400181@qq.com>
2023-07-29 21:57:23 +0800

    e

--- gamestates/City/CommandPanel.lua
+++ gamestates/City/CommandPanel.lua
@@ -2,13 +2,10 @@ CommandPanel = {}
 
 function CommandPanel:load()
     CurrentPlace.CommandPanelWindow = Window.new(64, 64, 432, 560)
-    CommandPanel = {
-        buffer = '',
-        commands = {}
-    }
-    CommandPanel.commands['give_money'] = function(amount)
-    end
-
+    -- CommandPanelin = {
+    --     buffer = '',
+    --     commands = {}
+    -- }
 end
 
 
@@ -16,26 +13,26 @@ function CommandPanel:update(dt)
 
 end
 
-function love.textinput(t)
-    CommandPanel.buffer = CommandPanel.buffer .. t
-end
-
-function love.keypressed(key)
-    if key == 'return' then
-        local commands, params = console.buffer:match('(%w+)%s*(.*)')
-        CommandPanel.commands[commands](params)
-        CommandPanel.buffer = ''
-    elseif key == 'backspace' then
-        CommandPanel.buffer = CommandPanel.buffer:sub(1, -2)
-    end
- end
-
-
 function CommandPanel:draw()
     CurrentPlace.CommandPanelWindow:use(
         function ()
-            love.graphics.draw(CityInfoPanel_screen)
+            love.graphics.draw(tank_info_panel)
             love.graphics.setColor(0,0,0)        
         end
     )
 end
+
+
+-- function love.textinput(t)
+--     CommandPanelin.buffer = CommandPanelin.buffer .. t
+-- end
+
+-- function love.keypressed(key)
+--     if key == 'return' then
+--         local commands, params = CommandPanelin.buffer:match('(%w+)%s*(.*)')
+--         CommandPanelin.commands[commands](params)
+--         CommandPanelin.buffer = ''
+--     elseif key == 'backspace' then
+--         CommandPanelin.buffer = CommandPanelin.buffer:sub(1, -2)
+--     end
+--  end
